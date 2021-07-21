74f8161af514ee990068b8845655086a01ef5cd3
trung <vanhuuthanhtrung@gmail.com>
2021-07-21 08:25:22 +0300

    test modifying app

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    const blogs = blogService.getAll()
+    console.log(blogs)
+    setBlogs(blogs)
   }, [])
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
